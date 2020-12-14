6c02406ad39469473be2bb2495914d654d16ba23
i19fukuda <i19fukuda1k@gmail.com>
2020-12-14 23:42:38 +0900

    編集スペースを初期化するメソッドを定義した

--- view/editSpace/editPane/EditSpase.java
+++ view/editSpace/editPane/EditSpase.java
@@ -1,5 +1,101 @@
 package view.editSpace.editPane;
 
+import java.util.ArrayList;
+
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.shape.Line;
+
 public class EditSpase {
-    
+    final private int BAR_WIDTH = 24 * 2 * 4;
+    // 4分音符は24tick
+    final private int QUAETER_NOTE_WIDTH = BAR_WIDTH/4;
+    final private int QUAETER_NOTE_HEIGHT = 25;
+
+    // スクロールペインの最大の大きさ
+    private int maxRootHeight = 6000;
+    private int maxRootWidth = 2500;
+    // 実際にユーザが入力するところ
+    private ScrollPane editSpaseRoot;
+    private AnchorPane editSpase;
+    // NoteRectを記憶し，呼び出し元に渡せるようにする．
+    private ArrayList<NoteRect> notes;
+
+    public EditSpase(){
+        init();
+    }
+
+    public void init(){
+        this.notes = new ArrayList<>();
+        this.editSpaseRoot = new ScrollPane();
+        this.editSpase = new AnchorPane();
+
+        ArrayList<Line> xLine = new ArrayList<>();
+        ArrayList<Line> yLine = new ArrayList<>();
+        ArrayList<Label> xLabel = new ArrayList<>();
+        ArrayList<Label> yLabel = new ArrayList<>();
+
+        // 縦線を引く
+        for(
+            int xPoint = 0;
+            xPoint< this.maxRootWidth;
+            xPoint+=this.QUAETER_NOTE_WIDTH / 4
+        ){
+            Line tmpLine = new Line(
+                xPoint, 0,
+                xPoint, this.maxRootHeight
+                );
+            if(xPoint % this.QUAETER_NOTE_WIDTH == 0){
+                // 4分音符の長さの線を少し太くする．
+            }
+            xLine.add(tmpLine);
+
+            Label tmpLabel = new Label(Integer.toString(xPoint));
+            AnchorPane.setTopAnchor(tmpLabel, 0.0);
+            AnchorPane.setLeftAnchor(tmpLabel,(double)xPoint);
+            xLabel.add(tmpLabel);
+        }
+
+        for(
+            int yPoint = 0;
+            yPoint< this.maxRootHeight;
+            yPoint+=this.QUAETER_NOTE_HEIGHT
+        ){
+            Line tmpLine = new Line(
+                0, yPoint,
+                this.maxRootWidth, yPoint
+                );
+            yLine.add(tmpLine);
+
+            Label tmpLabel = new Label(Integer.toString(yPoint));
+            AnchorPane.setTopAnchor(tmpLabel, 0.0);
+            AnchorPane.setLeftAnchor(tmpLabel,(double)yPoint);
+            yLabel.add(tmpLabel);
+        }
+
+        // 可変長配列に入ったLineを乗っけていく
+        for(Line l:xLine){
+            this.editSpase.getChildren().add(l);
+        }
+        for(Line l:yLine){
+            this.editSpase.getChildren().add(l);
+        }
+        for(Label l:xLabel){
+            this.editSpase.getChildren().add(l);
+        }
+        for(Label l:yLabel){
+            this.editSpase.getChildren().add(l);
+        }
+
+        this.editSpaseRoot.setContent(this.editSpase);
+    }
+
+    public ScrollPane getEditSpaseRoot(){
+        return this.editSpaseRoot;
+    }
+
+    public ArrayList<NoteRect> getNotes(){
+        return this.notes;
+    }
 }
