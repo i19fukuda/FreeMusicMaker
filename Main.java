3a08efb724619f08080444ac7a7fd5ac7a9a1c7f
i19fukuda <i19fukuda1k@gmail.com>
2021-01-04 21:42:30 +0900

    不要なファイルを削除

--- Main.java
+++ Main.java
@@ -1,19 +1,10 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import view.editSpace.editPane.EditSpase;
 import view.home.Home;
 
 public class Main extends Application{
-    EditSpase editer;
-    int tempo = 120;
-    int inst = 0;
-    TextField inTenpoField;
-    TextField insField;
-    TextField inSaveFileName;
-    TextField inLoadFileName;
     public static void main(String[] argas){
         Application.launch(argas);
     }
