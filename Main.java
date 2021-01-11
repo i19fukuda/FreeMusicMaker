1597311c1df894c8cae35866cf80e8c1e940a6fd
i19fukuda <i19fukuda1k@gmail.com>
2021-01-12 01:20:41 +0900

    不要なimportを削除

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
