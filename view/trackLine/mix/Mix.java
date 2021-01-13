537ea46933ddee571e3f2ff76191fa25cccc67bd
i19fukuda <i19fukuda1k@gmail.com>
2021-01-13 21:58:21 +0900

    ミックスをっスライドでできるようにした

--- view/trackLine/mix/Mix.java
+++ view/trackLine/mix/Mix.java
@@ -4,13 +4,14 @@ package view.trackLine.mix;
 import java.util.ArrayList;
 
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert.AlertType;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import view.editSpace.editPane.Note;
@@ -20,14 +21,15 @@ import view.trackLine.TrackLine;
 public class Mix {
     ArrayList<Note> notes;
     TrackLine line;
-    HBox ocRootHB;
+    VBox ocRootHB;
 
     Stage stage;
 
     int[] octPichs = {-36, -24, -12, 0, 12, 24, 36, 48};
     VBox[] octVB = new VBox[octPichs.length];
 
-    TextField[] inputFealFields;
+    Slider[] intputSliders;
+
     double[] volums;
 
 
@@ -35,30 +37,31 @@ public class Mix {
         this.notes = new ArrayList<>();
         this.line = line;
 
-        this.ocRootHB = new HBox();
+        this.ocRootHB = new VBox();
         this.stage = new Stage();
         this.stage.setTitle("Mix");
         this.stage.setWidth(800);
-        this.stage.setHeight(300);
+        this.stage.setHeight(500);
         Scene scene = new Scene(this.ocRootHB);
         stage.setScene(scene);
 
-        inputFealFields = new TextField[this.octPichs.length];
+        this.intputSliders = new Slider[this.octPichs.length];
         volums = new double[this.octPichs.length];
 
-        for(int i=0; i<inputFealFields.length; i++){
+        for(int i=0; i<this.intputSliders.length; i++){
             Label tmpLb = new Label(
                 Integer.toString(this.octPichs[i] / 12)
                 + "オクターブ"
             );
-            inputFealFields[i] = new TextField();
-            inputFealFields[i].setText("0");
-            inputFealFields[i].setOnAction(
-                event -> actionEventHandler(event)
-            );
+            intputSliders[i] = new Slider();
+            intputSliders[i].setMin(0);
+            intputSliders[i].setMax(100);
+            intputSliders[i].setShowTickLabels(true);
+            intputSliders[i].setShowTickMarks(true);
+            intputSliders[i].setValue(0);
 
             this.octVB[i] = new VBox();
-            this.octVB[i].getChildren().addAll(tmpLb, inputFealFields[i]);
+            this.octVB[i].getChildren().addAll(tmpLb, intputSliders[i]);
             this.ocRootHB.getChildren().add(this.octVB[i]);
         }
 
@@ -68,17 +71,13 @@ public class Mix {
     }
 
     private void setNotes(){
-        for(int i=0; i< this.inputFealFields.length; i++){
+        for(int i=0; i< this.intputSliders.length; i++){
             double vol;
-            try{
-                vol = Double.parseDouble(this.inputFealFields[i].getText());
-                if(vol>=100){
-                    this.showErrorDialog("out of range:" + vol);
-                }
-            } catch (NumberFormatException e){
-                this.showErrorDialog(e.getMessage());
-                vol = 100;
+            vol = this.intputSliders[i].getValue();
+            if(vol>=100){
+                this.showErrorDialog("Mix setNotes : out of range : " + vol);
             }
+
             this.volums[i] = vol * 0.01;
         }
 
@@ -115,23 +114,6 @@ public class Mix {
 
         for(Note note:createdNotes){
             this.notes.add(note);
-        }
-    }
-
-    private void actionEventHandler(ActionEvent event){
-        for(TextField field:this.inputFealFields){
-            try{
-                double imput = Double.parseDouble(field.getText());
-                if(
-                    imput > 100
-                    || imput<0
-                ){
-                    field.setText("0-100の数値");
-                }
-            } catch (NumberFormatException e){
-                showErrorDialog(e.getMessage());
-                field.setText("0-100の数値");
-            }
         }
     }
 
