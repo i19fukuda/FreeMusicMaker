b2e1017d84932b4e363afe70acc3cc801501a11d
i19fukuda <i19fukuda1k@gmail.com>
2021-01-20 11:04:24 +0900

    ドキュメントの追加

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"midi.conductor","h":"","u":""},{"l":"projectIo.projectLoad.anyTrackLoad","h":"","u":""},{"l":"projectIo.projectSave.anyTrackSave","h":"","u":""},{"l":"view.editSpace.editPane","h":"","u":""},{"l":"すべてのパッケージ","h":"","u":"allpackages-index.html"}]
---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"Conductor","h":"","u":""},{"l":"EditSpase","h":"","u":""},{"l":"LoadProject","h":"","u":""},{"l":"Main","h":"","u":""},{"l":"Note","h":"","u":""},{"l":"NoteRect","h":"","u":""},{"l":"SaveProject","h":"","u":""},{"l":"すべてのクラス","h":"","u":"allclasses-index.html"}]
